7fbf40d99c9cdd3e83c4a3e656cec8f9c9b37e08
Lukasz Skora <lukaszkskora@gmail.com>
2020-08-04 10:35:56 +0200

    comment

--- python/crash-course-on-python-(coursera)/group_list.py
+++ python/crash-course-on-python-(coursera)/group_list.py
@@ -7,10 +7,12 @@ Fill in the gaps in this function to do that.
 
 """
 
-def group_list(group, users):
-  members = ", ".join(users)
-  return group + ": " + members
 
-print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
-print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
-print(group_list("Users", "")) # Should be "Users:"
+def group_list(group, users):
+    members = ", ".join(users)
+    return group + ": " + members
+
+
+print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"]))  # Should be "Marketing: Mike, Karen, Jake, Tasha"
+print(group_list("Engineering", ["Kim", "Jay", "Tom"]))  # Should be "Engineering: Kim, Jay, Tom"
+print(group_list("Users", ""))  # Should be "Users:"
